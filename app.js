0fa187e433007101c83a28dc8babdf21ee8aae79
sunnixx <sumair.hamza@gmail.com>
2016-12-22 00:26:19 +0500

    user signup and database connection

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
 var express = require('express');
 var morgan = require('morgan');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var User = require('./models/user');
 
 var app = express();
 
-//Middleware for morgan
-app.use(morgan('dev'));
-
-app.get('/',function(req,res){
-  res.json("My name is Sumair");
+mongoose.connect('mongodb://root:abc123@ds141108.mlab.com:41108/ecommerce',function(err){
+  if(err){
+    console.log(err);
+  }else{
+    console.log('Connected to the database');
+  }
 });
+
+//Middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// Routes
+app.post('/create-user',function(req,res, next){
+  var user = new User();
+
+  user.profile.name = req.body.name;
+  user.password = req.body.password;
+  user.email = req.body.email;
+
+  user.save(function(err){
+    if(err) return next(err);
+
+    res.json('Successfully created a new user');
+  });
+
+});
+
+
 
 app.listen(3000,function(err){
   if(err) throw err;
