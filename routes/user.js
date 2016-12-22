08280f579f6bb5ad985a22a7b795da2a82765c51
sunnixx <sumair.hamza@gmail.com>
2016-12-22 15:40:05 +0500

    signup form with validation into database added

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,9 @@ var router = require('express').Router();
 var User = require('../models/user');
 
 router.get('/signup',function(req,res, next){
-  res.render('accounts/signup');
+  res.render('accounts/signup', {
+    errors: req.flash('errors')
+  });
 });
 
 router.post('/signup',function(req,res){
@@ -15,13 +17,13 @@ router.post('/signup',function(req,res){
 
   User.findOne({email: req.body.email}, function(err, existingUser){
     if(existingUser){
-      console.log(req.body.email + " is already existing");
+      req.flash("errors","Account with that email address already exist");
       return res.redirect('/signup');
     }else{
       user.save(function(err,user){
         if(err) return next(err);
 
-        res.json("New User has been created");
+        return res.redirect('/'); //This will be updated towards the profile page.
       });
     }
   });
